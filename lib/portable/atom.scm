8f156b5647a615dcfe9601d6fdb48c474490cf0a
Christian Stigen Larsen <csl@sublevel3.org>
2012-12-27 17:50:15 +0100

    Added atom->string

--- lib/portable/atom.scm
+++ lib/portable/atom.scm
@@ -1,6 +1,21 @@
-(define-library (portable atom)
+(define-library (portable atom?)
   (import (scheme base))
-  (export atom?)
+  (export
+    atom->string
+    atom?)
   (begin
+    ;; Atom predicate taken from The Little Schemer
     (define (atom? x)
-      (and (not (pair? x)) (not (null? x))))))
+      (and (not (pair? x)) (not (null? x))))
+
+    ;; Convert (almost) any atom to string
+    (define (atom->string x)
+      (cond
+        ((string? x) x)
+        ((number? x) (number->string x))
+        ((char? x) (list->string (list x)))
+        ((boolean? x) (boolean->string x))
+        ((vector? x) (vector->string x))
+        ((symbol? x) (symbol->string x))
+        (else
+          (error "Unsupported atom in atom->string"))))))
