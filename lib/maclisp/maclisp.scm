3b28c4e2af9780664a5c6aaeeb172977a4527728
Christian Stigen Larsen <csl@sublevel3.org>
2012-12-27 20:30:41 +0100

    Added aset' and using (portable atom)

--- lib/maclisp/maclisp.scm
+++ lib/maclisp/maclisp.scm
@@ -1,19 +1,34 @@
 #|
 
-Implementation of some MacLisp functions.
+Implementation of *some* MacLisp functions.
+
+They don't behave exactly the same was as in the original MacLisp.
 
 Copyright (C) 2012 Christian Stigen Larsen
 Distributed under the GNU LGPL 2.1; see LICENSE.
 
 |#
 (define-library (maclisp)
-  (import (scheme base))
+  (import (scheme base)
+          (portable atom))
   (export
+    aset'
     explode
     exploden
     implode)
   (begin
     (define base 8)
+
+    #|
+
+    Apparently same as set!, but returns the new value.
+
+    |#
+    (define-syntax aset'
+      (syntax-rules ()
+        ((_ a b) (begin
+                   (set! a b)
+                   a))))
 
     #|
 
@@ -26,17 +41,20 @@ Distributed under the GNU LGPL 2.1; see LICENSE.
     see http://www.maclisp.info/pitmanual/charac.html#11.3.1
 
     |#
-    (define (explode symbol)
+    (define (explode atom)
       (map string->symbol
         (map list->string
           (map list
             (string->list
-              (symbol->string symbol))))))
+              (atom->string atom))))))
 
-    ;; As (explode) but returns numbers.
-    ;;
-    (define (exploden symbol)
-      (map char->integer (string->list (symbol->string symbol))))
+    #|
+
+    As (explode) but returns numbers.
+
+    |#
+    (define (exploden atom)
+      (map char->integer (string->list (atom->string atom))))
 
     #|
 
@@ -47,8 +65,8 @@ Distributed under the GNU LGPL 2.1; see LICENSE.
     (implode (cdr (explode 'hello))) ==> 'ello
 
     |#
-    (define (implode symbols)
+    (define (implode atoms)
       (string->symbol
         (list->string
           (map car
-            (map string->list (map symbol->string symbols))))))
+            (map string->list (map atom->string atoms))))))
