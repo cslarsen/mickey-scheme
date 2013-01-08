05dfbff918133839fd8025900fc0342971221bf1
Christian Stigen Larsen <csl@viper.honeynor.no>
2013-01-08 07:16:01 +0100

    Better check for numerical limits

--- src/library/srfi-4.cpp
+++ src/library/srfi-4.cpp
@@ -11,6 +11,7 @@
 
 #include <iostream>
 #include <vector>
+#include <limits>
 #include <stdlib.h>
 #include <inttypes.h>
 #include "assertions.h"
@@ -19,13 +20,54 @@
 #include "print.h"
 
 /*
- * A SIGNED data type large enough to hold both signed and usigned 64-bit
- * numbers.
- *
- * If you do not have __int128_t on your system, use the closest SIGNED type
- * you can find, or submit a patch.
+ * Convert a number to given data type and check for numerical limits.
  */
-typedef __int128_t large_t;
+template<typename T>
+static T convert(const cons_t* n)
+{
+  if ( integerp(n) ) {
+    integer_t i = n->number.integer;
+
+    if ( i > std::numeric_limits<T>::max() ||
+         i < std::numeric_limits<T>::min() )
+    {
+      raise(runtime_exception("Integer does not fit target type"));
+    }
+
+    return static_cast<T>(i);
+  }
+
+  if ( realp(n) ) {
+    real_t f = n->number.real;
+
+    if ( f > std::numeric_limits<T>::max() ||
+         f < std::numeric_limits<T>::min() )
+    {
+      raise(runtime_exception("Real number does not fit target type"));
+    }
+
+    return static_cast<T>(f);
+  }
+
+  if ( rationalp(n) ) {
+    real_t f = static_cast<real_t>(n->number.rational.numerator) /
+               static_cast<real_t>(n->number.rational.denominator);
+
+    if ( f > std::numeric_limits<T>::max() ||
+         f < std::numeric_limits<T>::min() )
+    {
+      raise(runtime_exception("Rational number does not fit target type"));
+    }
+
+    // TODO: We shouldn't just truncate the number here
+    return static_cast<T>(n->number.rational.numerator /
+                          n->number.rational.denominator);
+  }
+
+  raise(runtime_exception(format("Cannot convert to integer type: %s",
+          sprint(n).c_str())));
+  return 0;
+}
 
 /*
  * Pointer tags (used internally to discern between void pointers).
@@ -40,33 +82,9 @@ typedef __int128_t large_t;
 #define TAG_U64 "u64vector"
 
 /*
- * Minimum and maximum values given data types can hold (assuming two's
- * complement).
- */
-#define MIN_S8 -128
-#define MAX_S8  127
-#define MIN_U8  0
-#define MAX_U8  255
- //
-#define MIN_S16 -32768
-#define MAX_S16  32767
-#define MIN_U16 0
-#define MAX_U16 65536
- //
-#define MIN_S32 -2147383647 /* -2^31 + 1 */
-#define MAX_S32  2147483647 /*  2^31 - 1 */
-#define MIN_U32 0
-#define MAX_U32 4294967295  /*  2^32 - 1 */
- //
-#define MIN_S64 -9223372036854775807 /* -2^63 + 1 */
-#define MAX_S64  9223372036854775807 /*  2^63 - 1 */
-#define MIN_U64 0
-#define MAX_U64 18446744073709551615UL /* 2^64 - 1 */
-
-/*
  * (make-<tag>vector size [fill])
  */
-#define MAKE_VECTOR(SHORT_TAG, PTR_TAG, DATA_TYPE, MIN_VAL, MAX_VAL) \
+#define MAKE_VECTOR(SHORT_TAG, PTR_TAG, DATA_TYPE) \
 extern "C" cons_t* make_ ## SHORT_TAG ## \
                    vector(cons_t* p, environment_t*) \
 { \
@@ -81,21 +99,7 @@ extern "C" cons_t* make_ ## SHORT_TAG ## \
   cons_t *f = cadr(p); \
   assert_number(f); \
   \
-  large_t n = 0; \
-  if ( integerp(f) )  n = f->number.integer; else \
-  if ( realp(f) )     n = f->number.real; else \
-  if ( rationalp(f) ) n = (DATA_TYPE) f->number.rational.numerator / \
-                          (DATA_TYPE) f->number.rational.denominator; \
-  else { \
-    raise(runtime_exception(format("Cannot fill %s with %s", \
-      PTR_TAG, to_s(type_of(f)).c_str()))); \
-  } \
-  \
-  if ( n<MIN_VAL || n>MAX_VAL ) \
-    raise(runtime_exception(format("Value %s is too large to fit in %s", \
-      sprint(f).c_str(), PTR_TAG))); \
-  \
-  DATA_TYPE fill = static_cast<DATA_TYPE>(n); \
+  DATA_TYPE fill = convert<DATA_TYPE>(f); \
   return pointer(PTR_TAG, new std::vector<DATA_TYPE>(size, fill)); \
 }
 
@@ -112,7 +116,7 @@ extern "C" cons_t* SHORT_TAG ## vectorp(cons_t* p, environment_t*) \
 /*
  * (<tag>vector ...)
  */
-#define VECTOR(SHORT_TAG, PTR_TAG, DATA_TYPE, MIN_VAL, MAX_VAL) \
+#define VECTOR(SHORT_TAG, PTR_TAG, DATA_TYPE) \
 extern "C" cons_t* SHORT_TAG ## vector(cons_t* p, environment_t*) \
 { \
   const size_t size = length(p); \
@@ -121,23 +125,7 @@ extern "C" cons_t* SHORT_TAG ## vector(cons_t* p, environment_t*) \
   std::vector<DATA_TYPE> *v = new std::vector<DATA_TYPE>(size); \
   \
   while ( !nullp(p) ) { \
-    cons_t *f = car(p); \
-    large_t n = 0; \
-    \
-    if ( integerp(f) )  n = f->number.integer; else \
-    if ( realp(f) )     n = f->number.real; else \
-    if ( rationalp(f) ) n = (large_t) f->number.rational.numerator / \
-                            (large_t) f->number.rational.denominator; \
-    else { \
-      raise(runtime_exception(format("Cannot put %s into %s", \
-        to_s(type_of(f)).c_str(), PTR_TAG))); \
-    } \
-    \
-    if ( n<MIN_VAL || n>MAX_VAL ) \
-      raise(runtime_exception(format("Value %s is too large to fit in %s", \
-        sprint(f).c_str(), PTR_TAG))); \
-    \
-    v->operator[](pos++) = static_cast<DATA_TYPE>(n); \
+    v->operator[](pos++) = convert<DATA_TYPE>(car(p)); \
     p = cdr(p); \
   } \
   \
@@ -183,7 +171,7 @@ extern "C" cons_t* SHORT_TAG ## vector_ref(cons_t* p, environment_t*) \
 /*
  * (<tag>vector-set! obj index value)
  */
-#define VECTOR_SET(SHORT_TAG, PTR_TAG, DATA_TYPE, MIN_VAL, MAX_VAL) \
+#define VECTOR_SET(SHORT_TAG, PTR_TAG, DATA_TYPE) \
 extern "C" cons_t* SHORT_TAG ## vector_set(cons_t* p, environment_t*) \
 { \
   assert_length(p, 3); \
@@ -195,17 +183,11 @@ extern "C" cons_t* SHORT_TAG ## vector_set(cons_t* p, environment_t*) \
     static_cast<std::vector<DATA_TYPE>*>(car(p)->pointer->value); \
   \
   int k = cadr(p)->number.integer; \
-  int val = caddr(p)->number.integer; \
   \
   if ( k<0 || static_cast<size_t>(k) >= v->size() ) \
     raise(runtime_exception(format("%s index out of range", PTR_TAG))); \
   \
-  large_t n = 0; \
-  if ( n<MIN_VAL || n>MAX_VAL ) \
-    raise(runtime_exception(format("Value %s is too large to fit in %s", \
-      sprint(caddr(p)).c_str(), PTR_TAG))); \
-  \
-  v->operator[](k) = static_cast<DATA_TYPE>(val); \
+  v->operator[](k) = convert<DATA_TYPE>(caddr(p)); /* note: val is integer */ \
   return unspecified(); \
 }
 
@@ -245,14 +227,14 @@ extern "C" cons_t* SHORT_TAG ## vector_to_list( \
   return r; \
 }
 
-MAKE_VECTOR(s8,  TAG_S8,    int8_t, MIN_S8,  MAX_S8);
-MAKE_VECTOR(u8,  TAG_U8,   uint8_t, MIN_U8,  MAX_U8);
-MAKE_VECTOR(s16, TAG_S16,  int16_t, MIN_S16, MAX_S16);
-MAKE_VECTOR(u16, TAG_U16, uint16_t, MIN_U16, MAX_U16);
-MAKE_VECTOR(s32, TAG_S32,  int32_t, MIN_S32, MAX_S32);
-MAKE_VECTOR(u32, TAG_U32, uint32_t, MIN_U32, MAX_U32);
-MAKE_VECTOR(s64, TAG_S64,  int64_t, MIN_S64, MAX_S64);
-MAKE_VECTOR(u64, TAG_U64, uint64_t, MIN_U64, MAX_U64);
+MAKE_VECTOR(s8,  TAG_S8,    int8_t);
+MAKE_VECTOR(u8,  TAG_U8,   uint8_t);
+MAKE_VECTOR(s16, TAG_S16,  int16_t);
+MAKE_VECTOR(u16, TAG_U16, uint16_t);
+MAKE_VECTOR(s32, TAG_S32,  int32_t);
+MAKE_VECTOR(u32, TAG_U32, uint32_t);
+MAKE_VECTOR(s64, TAG_S64,  int64_t);
+MAKE_VECTOR(u64, TAG_U64, uint64_t);
 
 VECTORP(s8,  TAG_S8,   int8_t);
 VECTORP(u8,  TAG_U8,  uint8_t);
@@ -263,14 +245,14 @@ VECTORP(u32, TAG_U32, uint32_t);
 VECTORP(s64, TAG_S64,  int64_t);
 VECTORP(u64, TAG_U64, uint64_t);
 
-VECTOR(s8,  TAG_S8,    int8_t, MIN_S8,  MAX_S8);
-VECTOR(u8,  TAG_U8,   uint8_t, MIN_U8,  MAX_U8);
-VECTOR(s16, TAG_S16,  int16_t, MIN_S16, MAX_S16);
-VECTOR(u16, TAG_U16, uint16_t, MIN_U16, MAX_U16);
-VECTOR(s32, TAG_S32,  int32_t, MIN_S32, MAX_S32);
-VECTOR(u32, TAG_U32, uint32_t, MIN_U32, MAX_U32);
-VECTOR(s64, TAG_S64,  int64_t, MIN_S64, MAX_S64);
-VECTOR(u64, TAG_U64, uint64_t, MIN_U64, MAX_U64);
+VECTOR(s8,  TAG_S8,    int8_t);
+VECTOR(u8,  TAG_U8,   uint8_t);
+VECTOR(s16, TAG_S16,  int16_t);
+VECTOR(u16, TAG_U16, uint16_t);
+VECTOR(s32, TAG_S32,  int32_t);
+VECTOR(u32, TAG_U32, uint32_t);
+VECTOR(s64, TAG_S64,  int64_t);
+VECTOR(u64, TAG_U64, uint64_t);
 
 VECTOR_LENGTH(s8,  TAG_S8,   int8_t);
 VECTOR_LENGTH(u8,  TAG_U8,  uint8_t);
@@ -290,14 +272,14 @@ VECTOR_REF(u32, TAG_U32, uint32_t);
 VECTOR_REF(s64, TAG_S64,  int64_t);
 VECTOR_REF(u64, TAG_U64, uint64_t);
 
-VECTOR_SET(s8,  TAG_S8,    int8_t, MIN_S8,  MAX_S8);
-VECTOR_SET(u8,  TAG_U8,   uint8_t, MIN_U8,  MAX_U8);
-VECTOR_SET(s16, TAG_S16,  int16_t, MIN_S16, MAX_S16);
-VECTOR_SET(u16, TAG_U16, uint16_t, MIN_U16, MAX_U16);
-VECTOR_SET(s32, TAG_S32,  int32_t, MIN_S32, MAX_S32);
-VECTOR_SET(u32, TAG_U32, uint32_t, MIN_U32, MAX_U32);
-VECTOR_SET(s64, TAG_S64,  int64_t, MIN_S64, MAX_S64);
-VECTOR_SET(u64, TAG_U64, uint64_t, MIN_U64, MAX_U64);
+VECTOR_SET(s8,  TAG_S8,    int8_t);
+VECTOR_SET(u8,  TAG_U8,   uint8_t);
+VECTOR_SET(s16, TAG_S16,  int16_t);
+VECTOR_SET(u16, TAG_U16, uint16_t);
+VECTOR_SET(s32, TAG_S32,  int32_t);
+VECTOR_SET(u32, TAG_U32, uint32_t);
+VECTOR_SET(s64, TAG_S64,  int64_t);
+VECTOR_SET(u64, TAG_U64, uint64_t);
 
 LIST_TO_VECTOR(s8,  TAG_S8,   int8_t);
 LIST_TO_VECTOR(u8,  TAG_U8,  uint8_t);
