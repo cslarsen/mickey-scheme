ac7cc8ce517a23d7b45743f7737b61d0d03f118f
Christian Stigen Larsen <csl@sublevel3.org>
2013-05-12 23:23:21 +0200

    Added more procedures

--- lib/unix/time.scm
+++ lib/unix/time.scm
@@ -1,19 +1,37 @@
 #|
 
-   UNIX time(3) and friends for Mickey Scheme
+UNIX time(3) and friends for Mickey Scheme
 
-   Copyright (C) 2013 Christian Stigen Larsen
-   Distributed under the LGPL 2.1; see LICENSE
+Copyright (C) 2013 Christian Stigen Larsen
+Distributed under the LGPL 2.1; see LICENSE
+
+Example usage:
+
+  > (import (unix time))
+  > (define now (time))
+  > (ctime now)
+  "Sun May 12 23:20:10 2013\n"
+  > (time->list (localtime now))
+  ((seconds 10) (minutes 20) (hours 23) (month-day 12) (month may) (year 2013)
+   (week-day sunday) (year-day 131) (dst? #t) (timezone CEST)
+   (utc-offset-seconds 7200))
+  > (cadr (assv 'year (time->list (localtime now))))
+  2013
 
 |#
+
 (define-library (unix time)
   (export
     ctime
     gmtime
+    integer->month
     localtime
+    month
+    month->integer
+    months
     time
     time-value
-    tm->alist)
+    time->list)
 
   (import
     (mickey library)
@@ -21,6 +39,36 @@
 
   (begin
     (open-internal-library "libunix-time.so" 'lazy 'global)
+
+    (define months
+      #(january
+        february
+        march
+        april
+        may
+        june
+        july
+        august
+        september
+        october
+        november
+        december))
+
+    (define (integer->month number)
+      "Return month with given number in the range 1 to 12."
+      (if (or (< number 1) (> number 12))
+          (error "Month number must be in the range 1 to 12.")
+          (vector-ref months (- number 1))))
+
+    (define (month->integer month)
+      "Return the given month's number, between 1 and 12."
+      (let next
+        ((number 1))
+        (if (eqv? month (integer->month number))
+          number
+          (if (< number 12)
+              (next (+ 1 number))
+              (error "Not a recognized month")))))
 
     ;; Calls time(3) and returns pointer to time_t value.
     ;;
@@ -30,5 +78,5 @@
     (define time-value (bind-procedure "proc_time_value"))
     (define localtime (bind-procedure "proc_localtime"))
     (define gmtime (bind-procedure "proc_gmtime"))
-    (define tm->alist (bind-procedure "proc_tm_to_alist"))
+    (define time->list (bind-procedure "proc_tm_to_alist"))
     (define ctime (bind-procedure "proc_ctime"))))
