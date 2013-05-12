b173626b2faf82836ebc61639e684ceaca81ddb8
Christian Stigen Larsen <csl@sublevel3.org>
2013-05-12 08:22:44 +0200

    Added header

--- lib/portable/atom.scm
+++ lib/portable/atom.scm
@@ -1,3 +1,13 @@
+#|
+
+   Part of Mickey R7RS Scheme
+
+   Copyright (C) 2011-2013 Christian Stigen Larsen
+
+   Distributed under GNU LGPL v2.1, LGPL 3.0, GPL 2.0 or GPL 3.0
+   See the file LICENSE for full text.
+
+|#
 (define-library (portable atom?)
   (import (scheme base))
   (export
@@ -11,11 +21,11 @@
     ;; Convert (almost) any atom to string
     (define (atom->string x)
       (cond
-        ((string? x) x)
-        ((number? x) (number->string x))
-        ((char? x) (list->string (list x)))
+        ((string?  x) x)
+        ((number?  x) (number->string x))
+        ((char?    x) (list->string (list x)))
         ((boolean? x) (boolean->string x))
-        ((vector? x) (vector->string x))
-        ((symbol? x) (symbol->string x))
+        ((vector?  x) (vector->string x))
+        ((symbol?  x) (symbol->string x))
         (else
           (error "Unsupported atom in atom->string"))))))
