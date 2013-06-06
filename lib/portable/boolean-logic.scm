53780b9331fa50625e64e3a4b1a86c75e56f84d6
Christian Stigen Larsen <csl@sublevel3.org>
2013-06-06 13:47:53 +0200

    Added boolean/or

--- lib/portable/boolean-logic.scm
+++ lib/portable/boolean-logic.scm
@@ -1,38 +1,52 @@
 #|
 
-Provides boolean logic operators.
+   Provides boolean logic operators.
 
-Copyright (C) 2013 Christian Stigen Larsen
+   Copyright (C) 2013 Christian Stigen Larsen
 
-Distributed under GNU LGPL v2.1, LGPL 3.0, GPL 2.0 or GPL 3.0
-See the file LICENSE for full text.
+   Distributed under GNU LGPL v2.1, LGPL 3.0, GPL 2.0 or GPL 3.0
+   See the file LICENSE for full text.
 
-Provides boolean logic operators such as boolean/and and friends.  These
-are procedural instead of syntactic, so they will eagerly evaluate all
-their arguments.
+   Provides boolean logic operators such as boolean/and and friends.  These
+   are procedural instead of syntactic, so they will eagerly evaluate all
+   their arguments.
 
-The reason for this module is that one cannot do
+   The reason for this module is that one cannot do
 
- (apply and (list #t #t #t))
+     (apply and (list #t #t #t))
 
-because and is a macro.  This module lets one instead use
+   because and is a macro.  This module lets one instead use
 
- (apply boolean/and (list #t #t #t))
+     (apply boolean/and (list #t #t #t))
 
-TODO:
+   TODO:
 
-- Add boolean=?
-- Add boolean/or
+     - Add boolean=?
 
 |#
 (define-library (portable boolean-logic)
-  (import (scheme base))
+  (import
+    (scheme base))
+
   (export
-    boolean/and)
+    boolean/and
+    boolean/or)
+
   (begin
-    (define (boolean/and . args)
-      (if (null? args)
-          #t
-          (if (car args)
-              (apply boolean/and (cdr args))
-              #f)))))
+
+    ;; Procedural version of "and".
+    ;;
+    (define (boolean/and . x)
+      (cond
+        ((null? x) #t)                        ; end?   all items were true
+        ((car x) (apply boolean/and (cdr x))) ; true?  next
+        (else #f)))                           ; false? stop and return false
+
+    ;; Procedural version of "or".
+    ;;
+    (define (boolean/or . x)
+      (cond
+        ((null? x) #f)                      ; end?   all items were false
+        ((car x) #t)                        ; true?  stop and return true
+        (else (apply boolean/or (cdr x))))) ; false? next
+))
