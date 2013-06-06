ffec886510538dfa3fd52d2d3f0b42366d0e51c9
Christian Stigen Larsen <csl@sublevel3.org>
2013-06-06 10:07:13 +0200

    Added missing (scheme file)

---
+++ lib/scheme/file.scm
@@ -0,0 +1,26 @@
+#|
+
+Mickey R7RS Scheme
+
+Copyright (C) 2013 Christian Stigen Larsen <csl@sublevel3.org>
+http://csl.sublevel3.org                              _
+                                                       \
+Distributed under the LGPL 2.1; see LICENSE            /\
+Please post bugfixes and suggestions to the author.   /  \_
+
+|#
+
+(define-library (scheme file)
+
+  (import (only (scheme base) define)
+          (mickey library))
+
+  (export
+    file-exists?
+    delete-file)
+
+  (begin
+    (open-internal-library "libscheme-file.so" 'global 'lazy)
+
+    (define file-exists? (bind-procedure "proc_file_existsp"))
+    (define delete-file (bind-procedure "proc_delete_file"))))
