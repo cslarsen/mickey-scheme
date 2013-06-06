26814e09df44f5a3b709866f238a0cbc254637ab
Christian Stigen Larsen <csl@sublevel3.org>
2013-06-06 21:21:19 +0200

    Added (scheme read)

---
+++ lib/scheme/read.scm
@@ -0,0 +1,31 @@
+#|
+
+Mickey R7RS Scheme
+
+Copyright (C) 2012-2013 Christian Stigen Larsen <csl@sublevel3.org>
+http://csl.sublevel3.org                              _
+                                                       \
+Distributed under the LGPL 2.1; see LICENSE            /\
+Please post bugfixes and suggestions to the author.   /  \_
+
+|#
+(define-library (scheme read)
+  (import (scheme base)
+          (scheme case-lambda)
+          (mickey library))
+
+  (export read)
+
+  (begin
+    (open-internal-library "libscheme-read.so" 'lazy 'global)
+    (define read-port (bind-procedure "proc_read_from_port"))
+
+    ;; (read) --> reads from current-input-port
+    ;; (read <port>) --> reads from given port
+    ;;
+    ;; Returns a list parsed from the input.
+    ;;
+    (define read
+      (case-lambda
+        (() (read-port (current-input-port)))
+        ((port) (read-port port))))))
---
+++ src/library/scheme-read.cpp
@@ -0,0 +1,49 @@
+/*
+ * Mickey Scheme
+ *
+ * Copyright (C) 2011-2013 Christian Stigen Larsen <csl@sublevel3.org>
+ * http://csl.sublevel3.org                              _
+ *                                                        \
+ * Distributed under the LGPL 2.1; see LICENSE            /\
+ * Please post bugfixes and suggestions to the author.   /  \_
+ *
+ */
+
+#include "mickey.h"
+
+extern "C" {
+
+cons_t* proc_read_from_port(cons_t* p, environment_t* e)
+{
+  assert_length(p, 1);
+  assert_type(PORT, car(p));
+
+  port_t* port = car(p)->port;
+
+  FILE *f = port->file();
+  std::string s;
+  int ch = ' ';
+
+  while ( ch != '\n' && !feof(f) ) {
+    s += ch;
+    ch = fgetc(f);
+  }
+
+  program_t *prog = parse(s, e);
+  return car(prog->root);
+
+  /*
+   * TODO: If you do this:
+   *
+   * > (read (current-input-port))
+   * (+ 1 2 3) (* 4 5 6)
+   *
+   * it should RETURN '(+ 1 2 3) and
+   * EVALUATE (* 4 5 6) the reader should
+   * stop parsing as soon as it is finished,
+   * so the loop above should check balanced
+   * parens. When balanced, it should bail out.
+   */
+}
+
+}
