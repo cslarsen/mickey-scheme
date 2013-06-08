109470eb46fec875c2fdd7767686e8ad68b36adb
Christian Stigen Larsen <csl@sublevel3.org>
2013-06-08 05:14:51 +0200

    Do not use vector->list and make-vector

--- lib/scheme/base/make-list.scm
+++ lib/scheme/base/make-list.scm
@@ -4,8 +4,13 @@
 ;; Distributed under the LGPL 2.1; see LICENSE
 
 (define (make-list k . fill)
-  (if (null? fill)
-    (vector->list (make-vector k #f))
-    (if (not (= 1 (length fill)))
-      (error "make-list takes exactly one or two parameters")
-      (vector->list (apply make-vector (cons k fill))))))
+  (if (and (not (null? fill))
+           (> (length fill) 1))
+    (error "make-list accepts exactly one or two parameters"))
+
+  (let
+    ((f (if (null? fill) #f
+            (car fill))))
+
+    (if (<= k 0) '()
+        (cons f (make-list (- k 1) f)))))
