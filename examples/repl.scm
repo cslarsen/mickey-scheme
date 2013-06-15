3d6a389b4878628b78a5c58d218b0917da7c1a62
Christian Stigen Larsen <csl@sublevel3.org>
2013-06-15 19:41:20 +0200

    Added some more bells and whistles

--- examples/repl.scm
+++ examples/repl.scm
@@ -4,20 +4,43 @@
         (scheme repl)
         (portable print)
         (examples lisp-quotes)
-        (scheme inquiry))
+        (scheme inquiry)
+        (scheme process-context)
+        (posix sysconf)
+        (posix signal))
 
 (println (implementation-name) " "
          (implementation-version))
 (println "Copyright (C) 2013 Christian Stigen Larsen")
 (println)
 (println (random-quote))
+(println)
 
-(define env (interaction-environment))
+(print "Running on " (os-type) " " (cpu-architecture))
+(if (and (sysconf '_SC_NPROCESSORS_CONF)
+         (sysconf '_SC_NPROCESSORS_ONLN))
+  (begin
+    (println " --- "
+             (sysconf '_SC_NPROCESSORS_ONLN)
+             "/"
+             (sysconf '_SC_NPROCESSORS_CONF)
+             " CPUs online")))
+(println)
 
-(let loop
-  ()
-  (let*
-    ((foo (print "> "))
-     (in (read)))
-    (println (eval in env)))
+(define (exit-repl s)
+  (println "\nExiting ...")
+  (exit))
+
+;; Install some signal handlers
+(signal 'sigint exit-repl)  ; CTRL+C
+(signal 'sigquit exit-repl) ; CTRL+D
+(signal 'sigterm exit-repl) ; for graceul exit
+
+;; REPL's environment
+(define repl-env (interaction-environment))
+
+;; REPL
+(let loop ()
+  (print ";# scheme> ")
+  (println (eval (read) repl-env))
   (loop))
