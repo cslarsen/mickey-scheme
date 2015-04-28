07461013cbbc9869ce0b539f94692bd547059101
Christian Stigen Larsen <csl@csl.name>
2015-04-28 20:52:13 +0200

    Adds tail-call-elimination test

---
+++ tests/programs/tail-call-elimination.scm
@@ -0,0 +1,25 @@
+;; Tests tail call elimination
+
+(import (scheme base)
+        (scheme write)
+        (test unit-test))
+
+(define (my-loop body count-down count-up)
+   (if (> count-down 0)
+    (begin
+      (body)
+      (my-loop body (- count-down 1) (+ count-up 1)))
+    (values count-down count-up)))
+
+(define *counter* 0)
+
+(define (do-something)
+  (set! *counter* (+ *counter* 1))
+  (display ""))
+
+(let
+  ((name "tail-call-elimination.scm"))
+
+  ;; Have my-loop recurse 60000 times, which should break the stack if tail
+  ;; call elimination is not implemented.
+  (test name (my-loop do-something 60000 0) (values 0 *counter*)))
