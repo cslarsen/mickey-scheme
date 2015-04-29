e9e76b79bde9593e134fae4b2d2a0a93b40b36bb
Christian Stigen Larsen <csl@csl.name>
2015-04-29 19:59:44 +0200

    Uses autoreconf, etc.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-echo "Generating autotools files; this may take a while ..."
-
-libtoolize=$(which glibtoolize)
-if ! [ -x "$libtoolize" ]; then
-  libtoolize=libtoolize
-fi
+echo "Generating autotools files"
 
 python make-library-m4.py lib > library.m4
-$libtoolize --copy
-aclocal -I m4 --install
-autoheader
-automake --include-deps --add-missing --copy
-autoconf
-echo "You may now run ./configure && make -j"
+autoreconf -i
